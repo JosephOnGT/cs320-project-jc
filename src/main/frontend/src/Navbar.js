35bf2b2d51fd2aaafd329e7714910eef8909d20b
JosephOnGT <122491589+JosephOnGT@users.noreply.github.com>
2024-10-10 16:50:44 -0600

    💻 4.3 Make a Navigation Bar

---
+++ src/main/frontend/src/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div>
+                <Link to="/">Home</Link>
+                {
+
+                }
+                <span> </span>
+                <Link to="/page2">Page 2</Link>
+            </div>
+        </nav>
+    );
+
+};
+export default Navbar;
